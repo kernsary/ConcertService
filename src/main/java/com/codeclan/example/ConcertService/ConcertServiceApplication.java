2e11f50bf0699dc65dee3cd4f8ee5186a21568ce
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-12-03 18:25:31 +0000

    initial commit

---
+++ src/main/java/com/codeclan/example/ConcertService/ConcertServiceApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.ConcertService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertServiceApplication.class, args);
+	}
+
+}
